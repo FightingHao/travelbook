338d6548d6716aaa6f0dbcd0a5fe1084af42eecc
FightingHao <496915484@qq.com>
2018-11-01 23:51:30 +0800

    首页

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+
+  },
+  startAccounts() {
+    wx.showLoading({
+      'title': '旅程即将开始'
+    })
+    setTimeout(() => {
+      wx.hideLoading()
+      wx.navigateTo({
+        url: '../accountBooks/accountBooks'
+      })
+    }, 600);
+  }
+})
