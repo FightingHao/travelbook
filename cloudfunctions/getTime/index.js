64fa2bfa05cb1a00c10b9623faa1f3f4640dcf87
FightingHao <496915484@qq.com>
2018-11-05 13:17:52 +0800

    getTime云函数

---
+++ cloudfunctions/getTime/index.js
@@ -0,0 +1,21 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  var date = new Date()
+  var seperator1 = "-"
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var strDate = date.getDate()
+  if (month >= 1 && month <= 9) {
+    month = "0" + month
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate
+  }
+  var currentdate = year + seperator1 + month + seperator1 + strDate
+  return currentdate
+}
